5514ac525e6f9c9b0babe619493c360e669008c8
AnTbot98 <ATbot98@digibox.com>
2021-10-13 22:50:36 -0500

    changed

--- mytmuxsetup.sh
+++ mytmuxsetup.sh
@@ -2,7 +2,7 @@
 
 cpdev=c++dev
 
-#cd cprogs/Project/ModuleLab/
+cd cprogs/modlab
 tmux new -s $cpdev -n C++Dev -d
 tmux new-window -n Term -t $cpdev
 tmux send-keys -t $cpdev:0 './edit.sh main' C-m
